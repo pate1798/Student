247693a816d78350429b3a01378b084064a6a002
pate1798 <50718444+pate1798@users.noreply.github.com>
2019-06-20 08:16:42 -0700

    Update Animal.java

--- src/animalshelter/Animal.java
+++ src/animalshelter/Animal.java
@@ -37,7 +37,8 @@ public class Animal {
     public String getName() {
         return name;
     }
-    public short adoptionFee(){
+    public short adoptionFee(){ /// java used in software development
+       
         return 0;
     }
 
